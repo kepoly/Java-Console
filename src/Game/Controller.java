dab01ed0fda00d38809a169ae109ceae19da7088
Matthew spada <spadamatthew@gmail.com>
2016-04-03 19:45:37 -0400

    Quick fix

--- src/Game/Controller.java
+++ src/Game/Controller.java
@@ -70,6 +70,7 @@ public class Controller {
     public void setPlayerBet(double playerBet) {
         if (playerBet > this.getBalance()) {
             playerBet = this.getBalance();
+            this.playerBet = playerBet;
         } else {
             this.playerBet = playerBet;
         }
